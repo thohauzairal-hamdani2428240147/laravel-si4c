48d3592672a85845c844c5cc8b64ea9d7ef59d8a
thohauzairal-hamdani2428240147 <thohauzairal-hamdani_2428240147@mhs.mdp.ac.id>
2026-05-11 12:48:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('fakultas.create');
 });
 
 Route::resource('fakultas', FakultasController::class);
